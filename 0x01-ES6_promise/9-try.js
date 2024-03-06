db611403f815378b6ddc23503c9393951f873da5
willyopera <willy4opera@gmail.com>
2024-03-06 18:28:37 -0500

    Added the 9-try.js file

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default function guardrail(mathFunction) {
+  const queue = []; // create a list queue
+
+  try { // try pushing to the queue
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err)); // Get error message if failed 
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
