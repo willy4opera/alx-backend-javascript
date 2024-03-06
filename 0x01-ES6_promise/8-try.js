926d782b712f2e1f6cc372a6c82a828517544622
willyopera <willy4opera@gmail.com>
2024-03-06 18:25:28 -0500

    8-try.js

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,5 +1,5 @@
 export default function divideFunction(numerator, denominator) {
-  if (denominator === 0) { //check if denominator is num type and is zero
+  if (denominator === 0) { // check if denominator is num type and is zero
     throw Error('cannot divide by 0');
   } else {
     return numerator / denominator;
