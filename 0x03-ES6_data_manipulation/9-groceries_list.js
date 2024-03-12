19d012724cdc8c5b2a6a61368636b6e5cb94bbf3
willyopera <willy4opera@gmail.com>
2024-03-12 18:53:35 -0400

    Added the 9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+/**
+ * Fetches a map of grocery names and their quantity.
+ * @author ODIONYE OBIAJULU W <https://github.com/willy4opera>
+ * @returns {Map<string, Number>}
+ */
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
+}
