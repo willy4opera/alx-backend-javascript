555180321c8f7df57a267104655f2d7a8941b478
willyopera <willy4opera@gmail.com>
2024-03-12 18:29:51 -0400

    Added the 6-set.js

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author ODIONYE OBIAJULU W <https://github.com/willy4opera>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
