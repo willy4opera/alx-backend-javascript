ecebc704f5e5e54c1249083d1e12344539b11e14
willyopera <willy4opera@gmail.com>
2024-04-30 16:59:26 -0400

    Added the 0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays message on the STDOUT.
+ * @param {String} text The message to display.
+ * @author Odionye Williams <https://github.com/willy4opera>
+ */
+const displayMessage = (text) => {
+  console.log(text);
+};
+
+module.exports = displayMessage;
