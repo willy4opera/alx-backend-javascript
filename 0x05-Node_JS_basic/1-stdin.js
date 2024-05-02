11150403b7492e4801ac5ca102736d78c7c7eb03
willyopera <willy4opera@gmail.com>
2024-05-02 18:33:49 -0400

    Debuuged requirement issues with1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("readable", () => {
+process.stdin.on('readable', () => {
   const myname = process.stdin.read();
 
   if (myname) {
@@ -8,6 +8,6 @@ process.stdin.on("readable", () => {
   }
 });
 
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
