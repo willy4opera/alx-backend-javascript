4015f3ccce256ed4dbeaa2566c12ad9154f2f018
willyopera <willy4opera@gmail.com>
2024-05-02 19:02:50 -0400

    Added the 6-http_express.js

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,14 @@
+const expressd = require('express');
+
+const app = expressd();
+const PORT = 1245;
+
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
+
+module.exports = app;
