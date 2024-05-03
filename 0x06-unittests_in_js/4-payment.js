ac2200f11693af74950ff8380277348703d2f1b4
willyopera <willy4opera@gmail.com>
2024-05-03 16:09:04 -0400

    Added the 4-payment.test.js

---
+++ 0x06-unittests_in_js/4-payment.js
@@ -0,0 +1,11 @@
+/**
+ * Payment module
+ */
+const Utils = require('./utils');
+
+const sendPaymentRequestToApi = (sumAmount, sumShipping) => {
+  const sumCost = Utils.calculateNumber('SUM', sumAmount, sumShipping);
+  console.log(`The total is: ${sumCost}`);
+};
+
+module.exports = sendPaymentRequestToApi;
