238cd67e38c53eb71cd8a5a0a869f1dc369c32d6
willyopera <willy4opera@gmail.com>
2024-05-03 14:04:35 -0400

    Added the 1-calcul.test.js

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @param {*} type 
+ * @param {*} a 
+ * @param {*} b 
+ * @returns 
+ */
+const calculateNumber = (type, a, b) => {
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
+  }
+  if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  }
+  if (type === 'DIVIDE') {
+    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
+  }
+  return 0;
+};
+
+module.exports = calculateNumber;
